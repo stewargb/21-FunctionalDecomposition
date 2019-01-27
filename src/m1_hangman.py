2e8cb209ff4cab89162f2288ae32528b3a8a48d9
stewargb <stewargb@rose-hulman.edu>
2019-01-27 15:03:55 -0500

    done i think it looked better before

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -18,7 +18,16 @@ with open('words.txt') as f:
 def main():
     word = random_word(words)
     print(word)
-    hangman_main(word)
+
+    n = int(input('please enter a number of trys: '))
+    blank = []
+    for _ in range(len(word)):
+        blank = blank + ['-']
+    print(blank)
+    win = len(word)
+    letters_correct = 0
+
+    hangman_main(word,n,blank,win,letters_correct)
     play_again()
 
 def random_word(words):
@@ -26,14 +35,7 @@ def random_word(words):
     item = words[r]
     return item
 
-def hangman_main(word):
-    n = int(input('please enter a number of trys: '))
-    blank = []
-    for _ in range(len(word)):
-       blank = blank + ['-']
-    print(blank)
-    win = len(word)
-    letters_correct = 0
+def hangman_main(word,n,blank,win,letters_correct):
     while True:
         guess = str(input('Please guess a letter: '))
         check = 0
@@ -48,12 +50,17 @@ def hangman_main(word):
             n = n-1
             print('Sorry! There are no',guess,'letters in the secret word. You have',n,'unsuccessful guesses left before you LOSE the game!')
         print(blank)
+        end = did_i_win(n,letters_correct,win,word)
+        if end == 1:
+            break
+        # I learnd that you can not return a break from a function as it gives you a break outside of a loop
+def did_i_win(n,letters_correct,win,word):
         if n == 0:
             print('You lose! The secret word was:',word)
-            break
+            return 1
         if letters_correct == win:
             print('Congratulations, you WIN! the word was:', word)
-            break
+            return 1
 def play_again():
     y = 'y'
     n = 'n'
